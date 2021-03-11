d26216946c07495e170627180ada0f60c6c6db65
Christoper Federico <christopherfederico@gmail.com>
2021-03-11 08:30:38 -0800

    update terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -39,7 +39,7 @@ variable "function_name" {
     default = "cloudbots-function"
 }
 
-variable "client_id" {
+variable "azure_client_id" {
     description = "The App Registration client ID"
     default = "xxxxx-xxxxx-xxxxxx-xxxxx"
 }
@@ -49,7 +49,7 @@ variable "output_email" {
     default = "a.person@acme.com"
 }
 
-variable "secret" {
+variable "azure_client_secret" {
     description = "The App Registration secret key"
     default = "xxxxxxxxx"
 }
@@ -79,7 +79,7 @@ variable "basic_auth_password" {
     default = "xxxxxxxxxxxxxxx"
 }
 
-variable "tenant" {
+variable "azure_tenant_id" {
     description = "Azure AD tenant ID"
     default = "xxxxxxxxxxxxxxx"
 }
